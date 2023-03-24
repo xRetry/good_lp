d1650f5ad6a8b51f5ca47c2ca1bda18d60186cc5
xRetry <marcoschwarz.at@gmail.com>
2023-03-24 20:31:23 +0100

    Make field of ConstraintReference public

--- src/constraint.rs
+++ src/constraint.rs
@@ -138,7 +138,7 @@ macro_rules! constraint {
 #[derive(Clone, PartialEq, Debug)]
 /// A constraint reference contains the sequence id of the constraint within the problem
 pub struct ConstraintReference {
-    pub(crate) index: usize,
+    pub index: usize,
 }
 
 #[cfg(test)]
